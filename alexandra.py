aabbdd9898f3539a255bfb4c12f2dc765e8fac81
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-03-12 20:29:29 +0000

    player uses alexandra module

--- alexandra.py
+++ alexandra.py
@@ -2,38 +2,67 @@ from json import dumps, load, loads
 from pubsub import create_channel, publish, subscribe, unsubscribe, \
                    get_message, get_all_messages, get_message_block
 from StringIO import StringIO
-from urllib2 import urlopen
+from urllib2 import build_opener, HTTPHandler, Request, urlopen
 
 class Queue:
-    def __init__(self, channel, topic):
+    def __init__(self, channel, topic, subscribe_world, alex):
         self._channel = channel
         self._q = subscribe(self._channel, topic)
+        self._subscribe_world = subscribe_world
+        self._alex = alex
+
+    def next(self):
+        if self._subscribe_world is True:
+            self._alex.update_world()
+        message = get_message(self._channel, self._q)
+        if message is not None:
+            return loads(message)
+        else:
+            return None
 
     def fetch_all(self):
         return map(loads, get_all_messages(self._channel, self._q))
 
 class Alexandra:
-    def __init__(self):
+    def __init__(self, subscribe_world=False):
         self._channel = create_channel()
         self._library_url = self._get_library_url()
         self.config = self._get_config()
+        self._subscribe_world = subscribe_world
+        self._world_queue = None
         self.world = None
-        self.world_queue = None
+        if self._subscribe_world is True:
+            self.next_tick()
+
+    def enter_in_library(self, data, path, content_type):
+        opener = build_opener(HTTPHandler)
+        request = Request(self._library_url + path, data)
+        request.add_header('Content-Type', content_type)
+        request.get_method = lambda: 'PUT'
+        opener.open(request)
 
     def subscribe(self, topic):
-        return Queue(self._channel, topic)
+        return Queue(self._channel, topic, self._subscribe_world, self)
 
     def publish(self, topic, message):
         publish(self._channel, topic, dumps(message))
 
     def next_tick(self):
-        if self.world_queue is None:
-            self.world_queue = subscribe(self._channel, 'world')
-        self.world = loads(get_message_block(self._channel, self.world_queue))
+        if self._world_queue is None:
+            self._init_world_queue()
+        self.world = loads(get_message_block(self._channel, self._world_queue))
+
+    def update_world(self):
+        new_world = get_message(self._channel, self._world_queue)
+        if new_world is not None:
+            self.world = loads(new_world)
 
     def get_library_file(self, path):
         url = self._library_url + path
         return StringIO(urlopen(url).read())
+
+    def _init_world_queue(self):
+        self._world_queue = subscribe(self._channel, 'world')
 
     def _get_library_url(self):
         library_url_queue = subscribe(self._channel, 'library_url')
--- player.py
+++ player.py
@@ -1,94 +1,43 @@
-from json import dumps, load, loads
-from pubsub import create_channel, publish, subscribe, unsubscribe, \
-                   get_message, get_message_block
+from alexandra import Alexandra
+from json import dumps
 from time import sleep
-from sys import exit
-from urllib2 import build_opener, HTTPHandler, Request, URLError, urlopen
 
 IMAGE_FILE = 'player_image.png'
 WIDTH = 50
 HEIGHT = 50
+DELTAS = {'left': (-5, 0), 'right': (5, 0), 'up': (0, -5), 'down': (0, 5)}
 
-def init():
-    channel = create_channel()
-    config = init_config(channel)
-    publish_info(config['library_url'])
-    position = (config['start_x'], config['start_y'])
-    send_movement(position, position, channel)
-    commands_queue = subscribe(channel, 'commands.player')
-    world_queue = subscribe(channel, 'world')
+def init(alex):
+    alex.enter_in_library(open(IMAGE_FILE).read(),
+                          '/player/player.png', 'image/png')
+    alex.enter_in_library(dumps({'width': WIDTH, 'height': HEIGHT}),
+                          '/player/player.json', 'application/json')
+    position = (alex.config['player_start_x'], alex.config['player_start_y'])
+    send_movement(position, position, alex)
+    commands_queue = alex.subscribe('commands.player')
 
-    return channel, commands_queue, world_queue, position, config
+    return commands_queue, position
 
-def init_config(channel):
-    queue = subscribe(channel, 'library_url')
-    sleep(1)
-    library_url = get_message(channel, queue)
-    if library_url is None:
-        print "No library url published, cannot fetch config"
-        exit(1)
-    unsubscribe(channel, queue, 'library_url')
-
-    config_url = library_url + '/config.json'
-    try:
-        config_file = urlopen(config_url)
-    except URLError:
-        print "No config file at %s" % (config_url,)
-        exit(1)
-    config = load(config_file)
-
-    return {'start_x': config['player_start_x'],
-            'start_y': config['player_start_y'],
-            'width': WIDTH,
-            'height': HEIGHT,
-            'library_url': library_url,
-            'deltas': {'left': (-5, 0), 'right': (5, 0),
-                       'up': (0, -5), 'down': (0, 5)}}
-
-def publish_info(library_url):
-    publish_to_url(open(IMAGE_FILE).read(),
-                   library_url + '/player/player.png',
-                   'image/png')
-    publish_to_url(dumps({'width': WIDTH, 'height': HEIGHT}),
-                   library_url + '/player/player.json',
-                   'application/json')
-
-def publish_to_url(data, url, content_type):
-    opener = build_opener(HTTPHandler)
-    request = Request(url, data)
-    request.add_header('Content-Type', content_type)
-    request.get_method = lambda: 'PUT'
-    opener.open(request)
-
-def main_loop(channel, commands_queue, world_queue, position, deltas):
+def main_loop(commands_queue, position, alex):
     while True:
-        world = loads(get_message_block(channel, world_queue))
-        if 'player_0' not in world:
-            continue
-        position = world['player_0']['position']
-        command = get_input(channel, commands_queue)
-        if command is not None:
-            do(command, position, deltas)
+        command = commands_queue.next()
+        if command is not None and 'player_0' in alex.world:
+            position = alex.world['player_0']['position']
+            do(command, position, alex)
+        sleep(alex.config['tick_seconds']/5.0)
 
-def do(command, position, deltas):
-    delta = deltas.get(command)
+def do(command, position, alex):
+    delta = DELTAS.get(command)
     if delta is None:
         return position
     new_position = (position[0] + delta[0], position[1] + delta[1])
-    send_movement(position, new_position, channel)
+    send_movement(position, new_position, alex)
 
-def send_movement(from_position, to_position, channel):
+def send_movement(from_position, to_position, alex):
     movement = {'entity': 'player', 'index': 0,
                 'from': from_position, 'to': to_position}
-    publish(channel, 'movement.player', dumps(movement))
+    alex.publish('movement.player', movement)
 
-def get_input(channel, commands_queue):
-    message = get_message(channel, commands_queue)
-    if message is not None:
-        return loads(message)
-    else:
-        return None
-
-channel, commands_queue, world_queue, position, config = init()
-main_loop(channel, commands_queue, world_queue,
-          position, config['deltas'])
+alex = Alexandra(subscribe_world=True)
+commands_queue, position = init(alex)
+main_loop(commands_queue, position, alex)
