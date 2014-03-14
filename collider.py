f099c8223e9d04fd32a4a46d85ef691c7a15b794
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-03-14 07:43:26 +0000

    collider no longer caches

--- collider.py
+++ collider.py
@@ -8,36 +8,32 @@ def add_collisions(movement, alex):
                  movement)
 
 def collisions(movement, entities_dict):
-    dimensions = {}
     entities = entities_dict.values()
-    detector = make_collision_detector(movement, alex, dimensions)
+    detector = make_collision_detector(movement, alex)
     return filter(lambda(x): x is not None, map(detector, entities))
 
-def make_collision_detector(movement, alex, dimensions):
+def make_collision_detector(movement, alex):
     moving_entity = movement['entity']
-    moving_rect = get_rect_for(moving_entity, movement['to'], alex, {})
-    return lambda(e): get_collision(e, moving_entity, moving_rect, alex,
-                                    dimensions)
+    moving_rect = get_rect_for(moving_entity, movement['to'], alex)
+    return lambda(e): get_collision(e, moving_entity, moving_rect, alex)
 
-def get_collision(entity_data, moving_entity, moving_rect, alex, dimensions):
+def get_collision(entity_data, moving_entity, moving_rect, alex):
     entity, position = entity_data['entity'], entity_data['position']
-    rect = get_rect_for(entity, position, alex, dimensions)
+    rect = get_rect_for(entity, position, alex)
     if entity != moving_entity and rect.intersects(moving_rect):
         return entity_data
     else:
         return None
 
-def get_rect_for(entity, position, alex, dimensions):
-    width, height = get_dimensions(entity, alex, dimensions)
+def get_rect_for(entity, position, alex):
+    width, height = get_dimensions(entity, alex)
     top_left = Vector.frompair(position)
     bottom_right = top_left.add(Vector(width, height))
     return Rect(top_left, bottom_right)
 
-def get_dimensions(entity, library_url, dimensions):
-    if entity not in dimensions:
-        data = loads(alex.get_library_file('/%s/%s.json' % (entity, entity)))
-        dimensions[entity] = (data['width'], data['height'])
-    return dimensions[entity]
+def get_dimensions(entity, library_url):
+    data = loads(alex.get_library_file('/%s/%s.json' % (entity, entity)))
+    return (data['width'], data['height'])
 
 alex = Alexandra(subscribe_world=True)
 movement_queue = alex.subscribe('movement.*')
