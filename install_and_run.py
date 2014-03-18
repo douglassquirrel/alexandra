bea2a9fbe270d5208028a94ec6eff42485364f7b
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-03-18 20:13:20 +0000

    separate install and run

---
+++ install_and_run.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+
+from os import listdir
+from os.path import join as pathjoin
+from shutil import copy, copytree
+from subprocess import call
+from tempfile import mkdtemp
+
+CONFIG_COMPONENTS = ['library', 'bootstrap']
+
+def full_path_listdir(d):
+    return [pathjoin(d, name) for name in listdir(d)]
+
+def copy_libraries_to(dest_dir):
+    map(lambda(p): copy(p, dest_dir), full_path_listdir('libraries'))
+
+def copy_config_to(dest_dir):
+    copy('config.json', dest_dir)
+
+def install_components(install_dir):
+    installed_components_dir = pathjoin(install_dir, 'components')
+    copytree('components', installed_components_dir)
+    map(copy_libraries_to, full_path_listdir(installed_components_dir))
+    map(lambda(c): copy_config_to(pathjoin(installed_components_dir, c)),
+        CONFIG_COMPONENTS)
+
+def install_infra(install_dir):
+    copy('run.py', install_dir)
+
+install_dir = mkdtemp(prefix='alexandra.')
+print 'Installing in %s' % (install_dir,)
+install_components(install_dir)
+install_infra(install_dir)
+call(pathjoin(install_dir, 'run.py'), cwd=install_dir)
--- run.py
+++ run.py
@@ -1,48 +1,23 @@
 #! /usr/bin/python
 
 from os import access, listdir, X_OK
-from os.path import isfile, join as pathjoin
-from shutil import copy, copytree
+from os.path import abspath, isfile, join as pathjoin
 from subprocess import Popen
-from tempfile import mkdtemp
 
-COMPONENTS = ['library', 'bootstrap', 'collider', 'umpire',
-              'world', 'client', 'walls', 'player']
-CONFIG_COMPONENTS = ['library', 'bootstrap']
-
-from random import shuffle
-shuffle(COMPONENTS)
-
-def full_path_listdir(d):
-    return [pathjoin(d, name) for name in listdir(d)]
-
-def copy_libraries_to(dest_dir):
-    map(lambda(p): copy(p, dest_dir), full_path_listdir('libraries'))
-
-def copy_config_to(dest_dir):
-    copy('config.json', dest_dir)
-
-def install_components(install_dir):
-    installed_components_dir = pathjoin(install_dir, 'components')
-    copytree('components', installed_components_dir)
-    map(copy_libraries_to, full_path_listdir(installed_components_dir))
-    map(lambda(c): copy_config_to(pathjoin(installed_components_dir, c)),
-        CONFIG_COMPONENTS)
+def abspath_listdir(d):
+    return [abspath(pathjoin(d, name)) for name in listdir(d)]
 
 def is_executable_file(f):
     return isfile(f) and access(f, X_OK)
 
-def start_component(component_name, run_dir):
-    component_dir = pathjoin(run_dir, 'components', component_name)
-    executable = filter(is_executable_file, full_path_listdir(component_dir))[0]
+def start_component(component_name):
+    component_dir = abspath(pathjoin('components', component_name))
+    executable = filter(is_executable_file, abspath_listdir(component_dir))[0]
     print "Executing %s" % (executable,)
     process = Popen(executable, cwd=component_dir)
     return process
 
-install_dir = mkdtemp(prefix='alexandra.')
-print 'Running in %s' % (install_dir,)
-install_components(install_dir)
-processes = map(lambda(c): start_component(c, install_dir), COMPONENTS)
+processes = map(start_component, listdir('components'))
 print 'Now running'
 raw_input('Press Enter to stop')
 map(lambda(p): p.kill(), processes)
