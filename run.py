b8414f9900170f2db68e581866691601cd73ab8b
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-03-19 19:56:36 +0000

    list components in game.json

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 
+from json import load
 from os import access, getcwd, listdir, X_OK
 from os.path import abspath, isfile, join as pathjoin
 from subprocess import Popen
@@ -10,14 +11,18 @@ def abspath_listdir(d):
 def is_executable_file(f):
     return isfile(f) and access(f, X_OK)
 
-def start_component(component_name):
+def start_component(component_name, index):
     component_dir = abspath(pathjoin('components', component_name))
     executable = filter(is_executable_file, abspath_listdir(component_dir))[0]
-    print "Executing %s" % (executable,)
-    process = Popen([executable, abspath(getcwd())], cwd=component_dir)
+    print "Executing %s number %d" % (executable, index)
+    process = Popen([executable, abspath(getcwd()), str(index)],
+                    cwd=component_dir)
     return process
 
-processes = map(start_component, listdir('components'))
+with open('game.json', 'r') as game_file:
+    game_data = load(game_file)
+components = game_data['components']
+processes = map(lambda(c): start_component(*c), components.items())
 print 'Now running'
 raw_input('Press Enter to stop')
 map(lambda(p): p.kill(), processes)
