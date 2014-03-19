8f74ecc5ab59934c0cf0e5314579dc15c6806e69
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-03-19 08:03:02 +0000

    publish movements in world

--- components/world/world.py
+++ components/world/world.py
@@ -5,13 +5,15 @@ from time import sleep
 
 def main_loop(alex):
     movement_queue = alex.subscribe('decision_movement.*')
-    world = {'tick': 0, 'entities': {}}
+    world = {'tick': 0, 'movements': [], 'entities': {}}
     while True:
         world['tick'] += 1
+        world['movements'] = []
         for m in movement_queue.fetch_all():
             entity, index, position = m['entity'], m['index'], m['to']
             name = '%s_%d' % (entity, index)
             world['entities'][name] = {'entity': entity, 'position': position}
+            world['movements'].append(m)
         alex.publish('world', world)
         sleep(alex.config['tick_seconds'])
 
