168a5b086e159992bccc3dc2dfe27dbbc36474b6
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-03-20 23:23:44 +0000

    Revert "multiple opponents (but wonky)"
    This reverts commit 2f4d8844a6a2d19be4252540e970d9c70ed22658.

--- components/player/player.py
+++ components/player/player.py
@@ -2,10 +2,7 @@
 
 from alexandra import Alexandra
 from json import dumps
-from sys import argv
 
-INDEX = int(argv[2])
-NAME = 'player_%d' % (INDEX,)
 WIDTH = 20
 HEIGHT = 20
 COLOUR = [0, 255, 255]
@@ -18,15 +15,15 @@ def init(alex):
     return alex.subscribe('commands.player')
 
 def start_if_not_present(alex):
-    if NAME not in alex.world['entities']:
+    if 'player_0' not in alex.world['entities']:
         position = (alex.config['player_start_x'],
                     alex.config['player_start_y'])
         send_movement(position, position, alex)
 
 def move(command, alex):
-    if NAME not in alex.world['entities']:
+    if 'player_0' not in alex.world['entities']:
         return
-    position = alex.world['entities'][NAME]['position']
+    position = alex.world['entities']['player_0']['position']
     new_position = apply_command(command, position)
     send_movement(position, new_position, alex)
 
@@ -35,7 +32,7 @@ def apply_command(command, position):
     return (position[0] + delta[0], position[1] + delta[1])
 
 def send_movement(from_position, to_position, alex):
-    movement = {'entity': 'player', 'index': INDEX,
+    movement = {'entity': 'player', 'index': 0,
                 'from': from_position, 'to': to_position}
     alex.publish('movement.player', movement)
 
--- run.py
+++ run.py
@@ -5,9 +5,6 @@ from os import access, getcwd, listdir, X_OK
 from os.path import abspath, isfile, join as pathjoin
 from subprocess import Popen
 
-def flatten(list_of_lists):
-    return sum(list_of_lists, [])
-
 def abspath_listdir(d):
     return [abspath(pathjoin(d, name)) for name in listdir(d)]
 
@@ -22,14 +19,10 @@ def start_component(component_name, index):
                     cwd=component_dir)
     return process
 
-def start_component_group(component_name, n):
-    return map(lambda(i): start_component(component_name, i), range(n))
-
 with open('game.json', 'r') as game_file:
     game_data = load(game_file)
 components = game_data['components']
-processes = flatten(map(lambda(c): start_component_group(*c),
-                        components.items()))
+processes = map(lambda(c): start_component(*c), components.items())
 print 'Now running'
 print 'Press Space in game window to start'
 raw_input('Press Enter in this window to stop\n')
