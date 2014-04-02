5aae5108e71709be3e0d8a387861aab5b918df16
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-04-02 08:27:49 +0100

    emcee starts game

--- emcee/emcee.py
+++ emcee/emcee.py
@@ -1,14 +1,43 @@
 #! /usr/bin/python
 
 from json import loads
+from os import listdir
+from os.path import abspath, join as pathjoin
 from pubsub import Connection
+from shutil import copy, copytree
+from subprocess import call
 from sys import argv
+from tempfile import mkdtemp
 
-def start_game(message):
+def full_path_listdir(d):
+    return [pathjoin(d, name) for name in listdir(d)]
+
+def copy_libraries_to(libraries_dir, dest_dir):
+    map(lambda(p): copy(p, dest_dir), full_path_listdir(libraries_dir))
+
+def install_game(game_dir, install_dir, libraries_dir, infra_dir):
+    game_json = pathjoin(game_dir, 'game.json')
+    copy(game_json, install_dir)
+
+    game_components_dir = pathjoin(game_dir, 'components')
+    installed_components_dir = pathjoin(install_dir, 'components')
+    copytree(game_components_dir, installed_components_dir)
+    map(lambda(d): copy_libraries_to(libraries_dir, d),
+        full_path_listdir(installed_components_dir))
+
+    map(lambda(f): copy(f, install_dir), full_path_listdir(infra_dir))
+
+def start_game(message, games_dir, libraries_dir, infra_dir):
     game_info = loads(message)
     name, id = game_info['name'], game_info['id']
-    print 'Starting game %s with id %s' % (name, id)
+    game_dir = abspath(pathjoin(games_dir, name))
+    install_dir = mkdtemp(prefix=id + '.')
 
+    print 'Running game %s in %s using id %s' % (name, install_dir, id)
+    install_game(game_dir, install_dir, libraries_dir, infra_dir)
+    call([pathjoin(install_dir, 'run.py'), id], cwd=install_dir)
+
+games_dir, libraries_dir, infra_dir = argv[1], argv[2], argv[3]
 connection = Connection('emcee')
 queue = connection.subscribe('game.wanted')
-connection.consume(queue, start_game)
+connection.consume(queue, lambda(m): start_game(m, games_dir, libraries_dir, infra_dir))
--- emcee/run_emcee.py
+++ emcee/run_emcee.py
@@ -1,13 +1,20 @@
 #! /usr/bin/python
 
-from os.path import join as pathjoin
+from os.path import abspath, join as pathjoin
 from shutil import copy
 from subprocess import call
 from tempfile import mkdtemp
 
 install_dir = mkdtemp(prefix='emcee.')
+games_dir = abspath(pathjoin('..', 'games'))
+libraries_dir = abspath('libraries')
+infra_dir = abspath('infra')
 print 'Installing emcee in %s' % (install_dir,)
+print 'Games in %s' % (games_dir,)
+print 'Libraries in %s' % (libraries_dir,)
+print 'Infrastructure files in %s' % (infra_dir,)
 
 copy('emcee.py', install_dir)
 copy(pathjoin('libraries', 'pubsub.py'), install_dir)
-call([pathjoin(install_dir, 'emcee.py')], cwd=install_dir)
+call([pathjoin(install_dir, 'emcee.py'), games_dir, libraries_dir, infra_dir],
+     cwd=install_dir)
