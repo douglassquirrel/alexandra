34db881007bf0dad3b7e870d36ab4e4a68b0fd3d
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-04-04 08:16:29 +0100

    Deploy pubsub_ws along with emcee

--- emcee/run_emcee.py
+++ emcee/run_emcee.py
@@ -2,7 +2,7 @@
 
 from os.path import abspath, join as pathjoin
 from shutil import copy
-from subprocess import call
+from subprocess import Popen
 from tempfile import mkdtemp
 
 install_dir = mkdtemp(prefix='emcee.')
@@ -15,6 +15,16 @@ print 'Libraries in %s' % (libraries_dir,)
 print 'Infrastructure files in %s' % (infra_dir,)
 
 copy('emcee.py', install_dir)
+copy('pubsub_ws.py', install_dir)
+copy('pubsub_ws_doc.html', install_dir)
 copy(pathjoin('libraries', 'pubsub.py'), install_dir)
-call([pathjoin(install_dir, 'emcee.py'), games_dir, libraries_dir, infra_dir],
-     cwd=install_dir)
+processes = []
+processes.append(Popen([pathjoin(install_dir, 'emcee.py'),
+                        games_dir, libraries_dir, infra_dir],
+                       cwd=install_dir))
+processes.append(Popen([pathjoin(install_dir, 'pubsub_ws.py'), infra_dir],
+                       cwd=install_dir))
+
+print 'Now running'
+raw_input('Press Enter to stop')
+map(lambda(p): p.kill(), processes)
