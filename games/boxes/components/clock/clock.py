3fece5c44d152c6328f6a5fdef2919a64aa1031a
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-04-24 21:38:15 +0100

    Clock self-consumes

--- games/boxes/components/clock/clock.py
+++ games/boxes/components/clock/clock.py
@@ -3,9 +3,9 @@
 from alexandra import Alexandra
 from time import sleep
 
-alex = Alexandra()
-counter = 0
-while True:
-    alex.publish('tick', counter)
-    counter += 1
+def do_tick(tick, alex):
     sleep(alex.config['tick_seconds'])
+    alex.publish('tick', tick + 1)
+
+alex = Alexandra()
+alex.consume('tick', do_tick, initial=lambda: do_tick(0, alex))
