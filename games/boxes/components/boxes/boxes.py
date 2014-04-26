c2fda31693a4f59fcfb2468bb72ee64947d843b9
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-04-26 11:07:11 +0100

    boxes game with immobile boxes

--- games/boxes/components/boxes/boxes.py
+++ games/boxes/components/boxes/boxes.py
@@ -2,10 +2,9 @@
 
 from alexandra import Alexandra
 from json import dumps
+from random import choice as randomchoice
 from sys import argv
 
-NAME = 'box'
-INDEX = 0
 WIDTH = 20
 HEIGHT = 20
 COLOUR = [25, 170, 0]
@@ -16,20 +15,37 @@ def init(alex):
                                  'colour': COLOUR}),
                           '/box/box.json',
                           'application/json')
+    initial_positions = set()
+    while len(initial_positions) < alex.config['number_boxes']:
+        initial_positions.add(random_start_position(alex))
+    return list(initial_positions)
 
-def move(world, alex):
-    if NAME not in world['entities']:
-        position = (alex.config['player_start_x'] + 19,
-                    alex.config['player_start_y'] - 1)
-        send_movement(position, position, world['tick'], alex)
-        return
+def random_start_position(alex):
+    max_width = (alex.config['field_width'] - alex.config['grid_start_x'])
+    grid_width = max_width / WIDTH
+    max_height = (alex.config['field_height'] - alex.config['grid_start_y'])
+    grid_height = max_height / HEIGHT
+    return (randomchoice(range(grid_width)) * WIDTH,
+            randomchoice(range(grid_height)) * HEIGHT)
 
-def send_movement(from_position, to_position, tick, alex):
+def move(world, alex, initial_positions):
+    for index in range(0, alex.config['number_boxes']):
+        move_box(index, world, alex, initial_positions)
+
+def move_box(index, world, alex, initial_positions):
+    name = 'box_%d' % (index,)
+    if name not in world['entities']:
+        position = initial_positions[index]
+        send_movement(index, position, position, world['tick'], alex)
+    else:
+        pass
+
+def send_movement(index, from_position, to_position, tick, alex):
     movement = {'tick': tick,
-                'entity': 'box', 'index': INDEX,
+                'entity': 'box', 'index': index,
                 'from': from_position, 'to': to_position}
     alex.publish('movement.box', movement)
 
 alex = Alexandra()
-init(alex)
-alex.consume('world', move)
+initial_positions = init(alex)
+alex.consume('world', lambda w, a: move(w, a, initial_positions))
