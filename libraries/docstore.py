326f2d9742d33782de82cf04a7485334c76b1ceb
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-05-16 13:10:53 +0100

    docstore wait until up

--- libraries/docstore.py
+++ libraries/docstore.py
@@ -1,9 +1,18 @@
+from time import sleep, time as now
 from urllib2 import build_opener, HTTPHandler, Request, URLError, urlopen
 
 class Docstore:
     def __init__(self, url):
         self._url = url
         self._cache = {}
+
+    def wait_until_up(self, timeout=5):
+        give_up = now() + timeout
+        while now() < give_up:
+            if self._is_up():
+                return True
+            sleep(0.1)
+        return False
 
     def put(self, data, path, content_type):
         opener = build_opener(HTTPHandler)
@@ -18,6 +27,12 @@ class Docstore:
             if path not in self._cache:
                 return None
         return self._cache[path]['data']
+
+    def _is_up(self):
+        try:
+            return 200 == urlopen(self._url + '/index.html').getcode()
+        except URLError:
+            return False
 
     def _fill_cache(self, path):
         url = self._url + path
