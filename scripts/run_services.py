1b72f74a6b1118bebed7b403cbace1e8b2eae6cb
Douglas Squirrel <douglas.squirrel@secretsales.com>
2014-05-20 11:41:52 +0100

    publish files at startup

--- scripts/run_services.py
+++ scripts/run_services.py
@@ -7,6 +7,8 @@ path.insert(0, abspath(pathjoin('..', 'libraries')))
 from docstore import connect as docstore_connect
 from installer import install
 from json import load
+from os import listdir
+from os.path import abspath, basename, isdir, isfile, join as pathjoin
 from pubsub import connect as pubsub_connect
 from sys import exit
 
@@ -14,6 +16,25 @@ def install_service(name, options, services_dir, libraries_dir, docstore_url):
     service_dir = abspath(pathjoin(services_dir, name))
     sources = [service_dir, libraries_dir]
     install(name, sources, options, 'services', docstore_url)
+
+def abspath_listdir(d):
+    return [abspath(pathjoin(d, name)) for name in listdir(d)]
+
+def publish_file(root, path, docstore):
+    url_path = path[len(root):]
+    with open(path, 'r') as f:
+        data = f.read()
+    docstore.put(data, url_path)
+
+def is_valid_file(filename):
+    return not basename(filename).startswith('.')
+
+def publish_dir(root, path, docstore):
+    contents = filter(is_valid_file, abspath_listdir(path))
+    files = filter(isfile, contents)
+    dirs = filter(isdir, contents)
+    map(lambda f: publish_file(root, f, docstore), files)
+    map(lambda d: publish_dir(root, d, docstore), dirs)
 
 services_dir = abspath(pathjoin('..', 'services'))
 games_dir = abspath(pathjoin('..', 'games'))
@@ -42,6 +63,10 @@ if docstore.wait_until_up() is False:
     print 'Could not start docstore'
     exit(1)
 docstore.put(pubsub_url, '/services/pubsub')
+
+docstore_alex = docstore_connect(docstore_url + '/alexandra')
+root = abspath('..')
+publish_dir(root, root, docstore_alex)
 
 print 'Now running'
 raw_input('Press Enter to stop\n')
